3e4a2699a72b040303ec8215cb993efbbe6f6b28
abelanico6@gmail.com <abelanico6@gmail.com>
2021-04-06 23:21:42 +0100

    Initial commit

--- apps/base-app/src/app/todo/todo.service.ts
+++ apps/base-app/src/app/todo/todo.service.ts
@@ -1,12 +1,11 @@
 import { Injectable, Logger } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
-import { TodoDTO, CreateTodoDTO, UpdateTodoDTO } from '@hapicrow-backend-demo/interfaces';
+import { TodoDTO, CreateTodoDTO, UpdateTodoDTO, CustomAPIType } from '@hapicrow-backend-demo/interfaces';
 import { TodoDocument, Todo } from '../../schemas/todo.schema';
-import { BaseService } from '../../utils/generics/BaseService.service';
-import { CustomAPIType } from '../../utils/types/shared.types';
-import { OperationType } from '../../utils/types/app.constants';
+import { OperationType } from '@hapicrow-backend-demo/utils';
 import { mapTodoToDTO } from './dto/todo.dto';
+import { BaseService } from '../utils/generics/BaseService.service'
 
 @Injectable()
 export class TodoService extends BaseService<TodoDocument> {
